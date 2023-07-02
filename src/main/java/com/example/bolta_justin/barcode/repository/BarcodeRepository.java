e874123df0be4b1f4e066d27092d4731f5da141a
Flimberkim <kws1111049@gmail.com>
2023-07-02 16:40:00 +0900

    #7 : Point related fix

--- src/main/java/com/example/bolta_justin/barcode/repository/BarcodeRepository.java
+++ src/main/java/com/example/bolta_justin/barcode/repository/BarcodeRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface BarcodeRepository extends JpaRepository<Barcode, Long> {
 
     Optional<Barcode> findById(Long barcodeId);
+    Optional<Barcode> findByBarcode(String barcode);
 }
